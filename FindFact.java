b3c27c96795f868edc240ca1967be1b2ba52990a
Daanish M <MM109@ford.com>
2024-09-13 11:21:21 +0530

    added 2 files

--- FindFact.java
+++ FindFact.java
@@ -1,6 +1,10 @@
 public class FindFact{
 
 public static int findFactorial(int num){
+if(num==1){
+return 1;
+}
+return n*findFactorial(num-1);
 }
 public static void main(String[] args){
 	int result= findFactorial(5);
