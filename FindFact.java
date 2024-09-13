61aeb3d29167862a62f6bf455e644dc566ab0958
Daanish M <MM109@ford.com>
2024-09-13 11:01:59 +0530

    added factorial

---
+++ FindFact.java
@@ -0,0 +1,8 @@
+public class FindFact{
+
+public static int findFactorial(int num){
+}
+public static void main(String[] args){
+	int result= findFactorial(5);
+	System.out.println("result is:"+result);
+}}
